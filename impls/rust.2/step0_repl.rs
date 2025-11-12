50f3c6c9e63bfe2fda18019e2fa960800d795689
Or Harpazi <harpazior@gmail.com>
2025-11-13 00:41:05 +0200

    Remove reference from Makefile; Break on EOF

--- impls/rust.2/step0_repl.rs
+++ impls/rust.2/step0_repl.rs
@@ -1,13 +1,17 @@
 use std::io;
+use std::io::Write;
 
+#[allow(non_snake_case)]
 fn READ(x: String) -> String {
     x
 }
 
+#[allow(non_snake_case)]
 fn EVAL(x: String) -> String {
     x
 }
 
+#[allow(non_snake_case)]
 fn PRINT(x: String) -> String {
     x
 }
@@ -22,10 +26,19 @@ fn rep(input: String) -> String {
 
 fn main(){
     loop {
+        print!("user> ");
+        io::stdout().flush().unwrap();
+
         let mut input = String::new();
-        println!("input:");
-        io::stdin().read_line(&mut input).expect("Failed to read input");
-        let output: String = rep(input);
-        println!("{}", output);
+        if let Ok(bytes_in) = io::stdin().read_line(&mut input) {
+            if bytes_in == 0 {
+                break; // 0 bytes = EOF
+            }
+            let output = rep(input);
+            print!("{}", output);
+        } else {
+            println!("Error reading input");
+            break;
+        }
     }
 }
